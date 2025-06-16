2e3841e0309227a267318753f99d71dc9e3dc9d3
Stan Bergevoet <stanbergevoet@gmail.com>
2025-06-16 01:14:30 -0400

    Add start of parser

---
+++ include/parser/Parser.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "DiagnosticsManager.h"
+#include "lexer/Token.h"
+#include "parser/ParseContext.h"
+#include <initializer_list>
+
+namespace chocopy {
+class Parser {
+public:
+  Parser(std::span<const Token> tokens, DiagnosticsManager& diagnostics_manager) : m_tokens(tokens), m_diag_manager(diagnostics_manager) {};
+
+  [[nodiscard]] std::unique_ptr<ProgramContext> parse() { return parseProgram(); };
+
+private:
+  friend class ParserTest;
+
+  [[nodiscard]] std::optional<std::reference_wrapper<const Token>> peek(const int n = 0) const;
+
+  std::optional<const Token> advance();
+
+  template <typename... TokenTypes>
+  [[nodiscard]] bool match(const TokenTypes&... token_types) {
+    const auto current_token = peek();
+
+    if (!current_token) {
+      return false;
+    }
+
+    if (((current_token->get().getType() == token_types) || ...)) {
+      advance();
+      return true;
+    }
+
+    return false;
+  }
+
+  [[nodiscard]] std::unique_ptr<ProgramContext> parseProgram();
+  [[nodiscard]] std::unique_ptr<TypedVarContext> parseTypedVar();
+  [[nodiscard]] std::unique_ptr<TypeContext> parseType();
+  [[nodiscard]] std::unique_ptr<VarDefContext> parseVarDef();
+  [[nodiscard]] std::unique_ptr<LiteralContext> parseLiteral();
+
+  std::span<const Token> m_tokens;
+  std::size_t m_current_idx{0};
+  DiagnosticsManager& m_diag_manager;
+};
+} // namespace chocopy
