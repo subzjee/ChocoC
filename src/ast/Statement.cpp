f5e76c96cf3fafd6c095207511a587e0455eef85
Stan Bergevoet <stanbergevoet@gmail.com>
2025-08-05 13:11:28 -0400

    [[ast]] Fix `classof` Statement for LLVM RTTI

--- src/ast/Statement.cpp
+++ src/ast/Statement.cpp
@@ -1,8 +1,9 @@
 #include "ast/Statement.h"
+#include "ast/ASTNode.h"
 #include "ast/Expression.h"
 
 namespace chocopy::ast {
 bool Statement::classof(const ASTNode* node) {
-  return llvm::isa<Expression>(node);
+  return llvm::isa<Expression>(node) || node->getKind() == NK_AssignmentStatement;
 }
 };
